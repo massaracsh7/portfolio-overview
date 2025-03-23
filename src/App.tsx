0f9c0d129517875a2cbc3f5452a86ed133c14528
massaracsh7 <fatum7@gmail.com>
2025-03-23 11:59:08 +0300

    feat: add assetsSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import PortfolioOverview from "./pages/PortfolioOverview";
+import { store } from "./store/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <PortfolioOverview />
-    </>
+    </Provider>
   );
 }
 
--- src/pages/PortfolioOverview.tsx
+++ src/pages/PortfolioOverview.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 
 import AddModal from "../components/AddModal";
-import styles from "../styles/PortfolioOverview.module.scss";
+import styles from "./PortfolioOverview.module.scss";
 import ActiveTable from "../components/ActiveTable";
 
 const PortfolioOverview: React.FC = () => {
-const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className={styles.container}>
-      <h1>Portfolio Overview</h1>
-      <button onClick={() => setIsModalOpen(true)}>Добавить</button>
-      <ActiveTable />
+      <header>
+        <h1>Portfolio Overview</h1>
+        <button onClick={() => setIsModalOpen(true)}>Добавить</button>
+      </header>
+      <main>
+        <ActiveTable />
+      </main>
       {isModalOpen && <AddModal onClose={() => setIsModalOpen(false)} />}
     </div>
   );
